0113c2421340d19f7aecfb01f20ff8eef8c701b5
akihiko47 <akihiko4701a@gmail.com>
2023-10-23 19:08:37 +0300

    Fixed grid optimiztion

--- myscripts.js
+++ myscripts.js
@@ -5,7 +5,7 @@ let height = canvas.height;
 
 let content = canvas.getContext('2d'); 
 
-let G = 150;
+let G = 500;
 
 let particles = [];
 
@@ -13,13 +13,13 @@ let msTotal = 0;
 let msPrev = window.performance.now();
 let msPassed = 16 / 1000;
 
-let maxParticles = 700; 
+let maxParticles = 1500; 
 let nowParticles = 0;
 
 let FramesCounter = 0;
 let MaxFrames = 4;
 
-let subSteps = 4;
+let subSteps = 10;
 
 let mouseX = 0;
 let mouseY = 0;
@@ -43,6 +43,12 @@ grd.addColorStop(0, "#11151d");
 // colors
 let wallsColor = "#282a36"
 
+// grid
+grid_size_x = 80;
+grid_size_y = Math.round(grid_size_x * 9 / 16);
+let grid = [];
+create_new_grid();
+
 class Particle {
     constructor(x, y, ax, ay, radius) {
         this.x_now = x;
@@ -55,9 +61,9 @@ class Particle {
         this.ay = ay;
         this.radius = radius;
         this.color = "rgb(" +
-            Math.abs(Math.sin(nowParticles / 600)) * 255 + "," +
-            Math.abs(Math.cos(nowParticles / 800)) * 255 + "," +
-            Math.abs(Math.sin(nowParticles / 3000)) * 255 + ")";
+            Math.abs(Math.sin(nowParticles / 1200)) * 255 + "," +
+            Math.abs(Math.cos(nowParticles / 1600)) * 255 + "," +
+            Math.abs(Math.sin(nowParticles / 6000)) * 255 + ")";
     }
     
     update(dt) {
@@ -72,11 +78,60 @@ class Particle {
 
         this.ax = 0;
         this.ay = 0;
+
+        this.add_to_grid();
     }
 
     accelerate(acc_x, acc_y) {
         this.ax += acc_x;
         this.ay += acc_y;
+    }
+
+    add_to_grid() {
+        let grid_x = Math.floor(this.x_now * grid_size_x / width);
+        let grid_y = Math.floor(this.y_now * grid_size_y / height);
+
+        if (grid_x >= 0 & grid_x < grid_size_x) {
+            if (grid_y >= 0 & grid_y < grid_size_y) {
+                grid[grid_y][grid_x].push(this);
+            }
+        }
+    }
+}
+
+function check_grid() {
+    for (let x = 1; x < grid_size_y - 1; x++) {
+        for (let y = 1; y < grid_size_x - 1; y++) {
+
+            let current_cell = grid[x][y];
+
+            for (let dx = -1; dx <= 1; dx++) {
+                for (let dy = -1; dy <= 1; dy++) {
+                    let other_cell = grid[x + dx][y + dy];
+                    check_cells_collisions(current_cell, other_cell);
+                }
+            }
+        }
+    }
+}
+
+function create_new_grid() {
+    grid = new Array(grid_size_y);
+    for (let i = 0; i < grid_size_y; i++) {
+        grid[i] = new Array(grid_size_x);
+        for (let j = 0; j < grid_size_x; j++) {
+            grid[i][j] = new Array();
+        }
+    }
+}
+
+function check_cells_collisions(cell_1, cell_2) {
+    for (let particle1 of cell_1) {
+        for (let particle2 of cell_2) {
+            if (particle1 !== particle2) {
+                handle_between_collision(particle1, particle2);
+            }
+        }
     }
 }
 
@@ -85,11 +140,11 @@ function add_particles() {
         if (FramesCounter < MaxFrames) {
             FramesCounter += 1;
         } else {
-            let ax = 120000 * Math.cos(msTotal);
-            let ay = 120000 * Math.abs(Math.sin(msTotal));
-            particles.push(new Particle(width/2 + 100, height/4, ax, ay, (Math.random() + 0.5) * 12));
-            particles.push(new Particle(width/2, height/4, ax, ay, (Math.random() + 0.5) * 12));
-            particles.push(new Particle(width/2 - 100, height/4, ax, ay, (Math.random() + 0.5) * 12));
+            let ax = 240000 * Math.cos(msTotal);
+            let ay = 240000 * Math.abs(Math.sin(msTotal));
+            particles.push(new Particle(width/2 + 100, height/4, ax, ay, Math.random()*3 + 8));
+            particles.push(new Particle(width/2, height/4, ax, ay,  Math.random()*3 + 8));
+            particles.push(new Particle(width/2 - 100, height/4, ax, ay,  Math.random()*3 + 8));
             nowParticles += 3;
             FramesCounter = 0;
         }
@@ -190,30 +245,22 @@ function apply_constraint() {
     }
 }
 
-function handle_between_collision() {
-    for (let particle1 of particles) {
-        for (let particle2 of particles) {
-            if (particle1 === particle2) {
-                continue;
-            }
-    
-            let col_axis_x = particle1.x_now - particle2.x_now;
-        let col_axis_y = particle1.y_now - particle2.y_now;
+function handle_between_collision(particle1, particle2) {
+    let col_axis_x = particle1.x_now - particle2.x_now;
+    let col_axis_y = particle1.y_now - particle2.y_now;
 
-	    let dist = Math.sqrt(col_axis_x**2 + col_axis_y**2);
+    let dist = Math.sqrt(col_axis_x**2 + col_axis_y**2);
 
-	    if (dist < particle1.radius + particle2.radius) {
-		let n_x = col_axis_x / dist;
-		let n_y = col_axis_y / dist;
+    if (dist < particle1.radius + particle2.radius) {
+        let n_x = col_axis_x / dist;
+        let n_y = col_axis_y / dist;
 
-		let delta = particle1.radius + particle2.radius - dist;
+        let delta = particle1.radius + particle2.radius - dist;
 
-		particle1.x_now += 0.5 * delta * n_x;
-		particle1.y_now += 0.5 * delta * n_y;
-		particle2.x_now -= 0.5 * delta * n_x;
-		particle2.y_now -= 0.5 * delta * n_y;
-	    }
-        }
+        particle1.x_now += 0.5 * delta * n_x;
+        particle1.y_now += 0.5 * delta * n_y;
+        particle2.x_now -= 0.5 * delta * n_x;
+        particle2.y_now -= 0.5 * delta * n_y;
     }
 }
 
@@ -256,8 +303,9 @@ function main() {
     for (let i = 0; i < subSteps; i++) {
         apply_gravity();
         apply_constraint();
-        handle_between_collision();
+        create_new_grid();
         update_positions(msPassed / subSteps);
+        check_grid();
     }
 
     draw_particles();
